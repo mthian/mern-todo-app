e1c46ec64a10b12bcf14262c2c26fa544fecf3fb
treehugs <c2m2b@ugrad.cs.ubc.ca>
2021-01-03 16:41:46 -0800

    added routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css"
+import { BrowserRouter as Router, Route, Link} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import CreateTodo from "./components/create-todo.component";
+import TodosList from "./components/todos-list.component";
+import EditTodo from "./components/edit-todo.component";
+
+class App extends Component {
+    render() {
+        return (
+            <Router>
+                <div className={"container"}>
+                    <h2>MERN-Stack Todo App</h2>
+                    <Route path={"/"} exact component={TodosList}/>
+                    <Route path={"/edit/:id"} component={EditTodo}/>
+                    <Route path={"/create"} component={CreateTodo}/>
+                </div>
+            </Router>
+        );
+    }
 }
 
 export default App;
