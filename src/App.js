4a50468d3719736c0786d4edb2c5804c21946bf6
treehugs <c2m2b@ugrad.cs.ubc.ca>
2021-01-03 20:34:59 -0800

    created todo form

--- src/App.js
+++ src/App.js
@@ -7,12 +7,31 @@ import CreateTodo from "./components/create-todo.component";
 import TodosList from "./components/todos-list.component";
 import EditTodo from "./components/edit-todo.component";
 
+import logo from "./logo.svg";
+
 class App extends Component {
     render() {
         return (
             <Router>
                 <div className={"container"}>
-                    <h2>MERN-Stack Todo App</h2>
+                    <nav className={"navbar navbar-expand-lg navbar-light bg-light"}>
+                        <a className={"navbar-brand"} href={"https://google.com"} target={"_blank"}>
+                            <img src={logo} width={"30"} height={"30"} alt={"google page lmao"} />
+                        </a>
+                        <Link to={"/"} className={"navbar-brand"}> MERN-Stack Todo App</Link>
+                        <div className={"collapse navbar-collapse"}>
+                            <ul className={"navbar-nav mr-auto"}>
+                                <li className={"navbar-item"}>
+                                    <Link to={"/"} className={"nav-link"}>Todos</Link>
+                                </li>
+                                <li className={"navbar-item"}>
+                                    {/*<a href={"./create"}>Create Todos</a>*/}
+                                    {/*href loads a NEW page while Link makes the CURRENT page dynamic*/}
+                                    <Link to={"/create"} className={"nav-link"}>Create Todos</Link>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav>
                     <Route path={"/"} exact component={TodosList}/>
                     <Route path={"/edit/:id"} component={EditTodo}/>
                     <Route path={"/create"} component={CreateTodo}/>
